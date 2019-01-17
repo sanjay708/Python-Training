1332bcf44ee6bd0e63622a1c30f2bc40b3e02666
Sanjay Chauhan <chausan@ad.issgovernance.com>
2019-01-17 17:42:21 +0530

    py thrm

--- Demo.py
+++ Demo.py
@@ -1,2 +1,7 @@
-print("Sanjay")
-print("Abnish")
+from math import sqrt
+
+print("Input lengths of shorter triangle sides:")
+a = float(input("a: "))
+b = float(input("b: "))
+c = sqrt(a**2 + b**2)
+print("Hypotenuse is", c )
